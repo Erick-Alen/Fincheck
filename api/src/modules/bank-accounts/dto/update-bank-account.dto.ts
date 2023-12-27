abd0eff6e64b0458c37efc76599951b6eecbcc62
Erick Alen <erick.alen@live.com>
2023-12-27 17:25:10 -0300

    feat: added BankAccount module complete w/ CRUD opertations

---
+++ api/src/modules/bank-accounts/dto/update-bank-account.dto.ts
@@ -0,0 +1,5 @@
+import { CreateBankAccountDto } from './create-bank-account.dto';
+
+export class UpdateBankAccountDto extends CreateBankAccountDto {
+  
+}
--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -2,13 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
 import { CategoriesRepository } from './repositories/categories.repository';
+import { BankAccountsRepository } from './repositories/bank-accounts.repository';
 
 //module marked as global is visible to all modules in the application
 @Global()
 @Module({
-  providers: [UsersRepository, PrismaService, CategoriesRepository],
+  providers: [UsersRepository, PrismaService, CategoriesRepository, BankAccountsRepository],
   //module providers are ALWAYS PRIVATE, so we need to export users repo
-  exports:[UsersRepository, CategoriesRepository]
+  exports:[UsersRepository, CategoriesRepository, BankAccountsRepository]
 })
 
 export class DatabaseModule {}
