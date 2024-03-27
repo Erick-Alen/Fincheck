2396d325bf0a864fdb47d0c6c91ff70dc9f1a408
Erick Alen Ricioli <ricioli@weg.net>
2024-03-27 15:37:45 -0300

    fix: rename 'expense' type to 'outcome'

--- frontend/src/view/components/icons/categories/CategoryIcon.tsx
+++ frontend/src/view/components/icons/categories/CategoryIcon.tsx
@@ -1,14 +1,16 @@
-import { iconsMap } from "./iconsMap";
+import { iconsMap } from './iconsMap';
 
 interface CategoryIconProps {
-  type: 'income' | 'expense';
+  type: 'income' | 'outcome';
   category?: string;
 }
 
 export function CategoryIcon({ type, category }: CategoryIconProps) {
-  const Icon = iconsMap[type][
-    category as keyof (typeof iconsMap.expense | typeof iconsMap.income) ?? 'default'
-  ] ?? iconsMap[type].default;
+  const Icon =
+    iconsMap[type][
+      (category as keyof (typeof iconsMap.outcome | typeof iconsMap.income)) ??
+        'default'
+    ] ?? iconsMap[type].default;
 
-  return <Icon />
+  return <Icon />;
 }
