02c8b79827ff188084b9a4ebe7d8c242ca87f793
Erick Alen Ricioli <ricioli@weg.net>
2024-03-27 15:40:07 -0300

    fix: renamed bank accounts service

--- frontend/src/app/services/bankAccountService/delete.ts
+++
@@ -1,7 +0,0 @@
-import { httpClient } from '../httpClient';
-
-export const remove = async (bankAccountId: string) => {
-  // await sleep()
-  const { data } = await httpClient.delete(`/bank-accounts/${bankAccountId}`);
-  return data;
-}
