c2eac6a82831494d55f714fa66a8f2e185f19903
Erick Alen Ricioli <ricioli@weg.net>
2024-03-28 12:47:18 -0300

    feat: update custom hooks

--- frontend/src/app/hooks/useBankAccounts.tsx
+++ frontend/src/app/hooks/useBankAccounts.tsx
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { bankAccountsService } from '../services/bankAccountsService';
 
 export const useBankAccounts = () => {
-  const { data, isFetching } = useQuery({
+  const { data, isFetching, refetch } = useQuery({
     queryKey: ['bankAccounts'],
     queryFn: bankAccountsService.getAll,
+    enabled: true
   });
 
-  return { accounts: data ?? [], isFetching };
+  return { accounts: data ?? [], isFetching, refetch };
 };
--- frontend/src/app/hooks/useTransactions.ts
+++ frontend/src/app/hooks/useTransactions.ts
@@ -3,10 +3,10 @@ import { transactionsService } from '../services/transactionsService';
 import { TransactionsFilters } from '../services/transactionsService/getAll';
 
 export const useTransactions = (filters: TransactionsFilters) => {
-  const { data, isPending, isLoading, refetch, isFetching } = useQuery({
+  const { data, isPending, refetch, isFetching } = useQuery({
     queryKey: ['transactions'],
     queryFn: () => transactionsService.getAll(filters),
   });
 
-  return { transactions: data ?? [], isPending, isLoading, refetch, isFetching };
+  return { transactions: data ?? [], isPending, refetch, isFetching };
 };
